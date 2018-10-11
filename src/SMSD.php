dfc546d1d74c4d1d204592d3bf1359a2a2fde7f5
Alexander K. Burko <aleksandr_burko@mail.ru>
2018-10-11 17:57:50 +0700

    Получение списка входящих смс-сообщений

--- src/SMSD.php
+++ src/SMSD.php
@@ -18,10 +18,9 @@ class SMSD
      */
     public function getInboxList()
     {
-//        $stmt = $this->_db->prepare("SELECT * FROM `inbox` limit 10");
-//        $stmt->execute();
-        $data = $this->_db->query('SELECT * FROM `inbox` LIMIT 10')->fetchAll(PDO::FETCH_KEY_PAIR);
-        return json_encode($data);
+        $stmt = $this->_db->prepare("SELECT * FROM `inbox` limit 10");
+        $stmt->execute();
+        return json_encode($stmt->fetchAll());
     }
 
     /**
